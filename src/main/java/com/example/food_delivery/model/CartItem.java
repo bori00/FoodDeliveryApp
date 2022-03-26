d69f2b52f2299aaffb1ed178349d60658014c843
bori00 <fazakasbori@gmail.com>
2022-03-26 02:07:46 +0200

    define CartItem

---
+++ src/main/java/com/example/food_delivery/model/CartItem.java
@@ -0,0 +1,31 @@
+package com.example.food_delivery.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="cart_item")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CartItem {
+    @javax.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @ManyToOne(optional = false, cascade = CascadeType.MERGE)
+    @JoinColumn(name = "customer_id", referencedColumnName = "Id")
+    private Customer customer;
+
+    @ManyToOne(optional = false, cascade = CascadeType.MERGE)
+    @JoinColumn(name = "food_id", referencedColumnName = "Id")
+    private Food food;
+}
