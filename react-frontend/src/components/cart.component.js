111a0e9cc900f618424ceb7d771e51604157cb5c
bori00 <fazakasbori@gmail.com>
2022-04-03 14:13:11 +0300

    allow customers to place orders if they have a non-empty cart

--- react-frontend/src/components/cart.component.js
+++ react-frontend/src/components/cart.component.js
@@ -14,7 +14,8 @@ export default class Cart extends Component {
             message: "",
             restaurantName: "",
             itemsToQuantity: {},
-            totalPrice: 0
+            totalPrice: 0,
+            emptyCart: true
         };
     }
 
@@ -23,12 +24,22 @@ export default class Cart extends Component {
             .then(response => {
                 if (response.ok) {
                     response.json().then( responseCart => {
-                        this.setState({
-                            restaurantName: responseCart.restaurantName,
-                            loading: false,
-                            itemsToQuantity: responseCart.selectedItemsToQuantity,
-                            totalPrice: this.getTotalPrice(responseCart.selectedItemsToQuantity)
-                        });
+                        console.log(responseCart)
+                        if (responseCart.restaurantName != null) {
+                            this.setState({
+                                restaurantName: responseCart.restaurantName,
+                                loading: false,
+                                itemsToQuantity: responseCart.selectedItemsToQuantity,
+                                totalPrice: this.getTotalPrice(responseCart.selectedItemsToQuantity),
+                                emptyCart: false
+                            });
+                        } else {
+                            console.log("EMPTY")
+                            this.setState({
+                                loading: false,
+                                emptyCart: true
+                            });
+                        }
                     });
                 } else {
                     response.json().then(response => response.messages.join("\n")).then(errorMsg => {
@@ -73,14 +84,21 @@ export default class Cart extends Component {
 
     render() {
 
-        const {loading, message, restaurantName, itemsToQuantity, totalPrice} = this.state;
-
         console.log("State: ", this.state)
 
-        const itemComponentList = Object.entries(itemsToQuantity).map(function(item) {
-            console.log("Item: ", item[0])
-            return <CartItem key={JSON.parse(item[0]).name} foodItem={item[0]} quantity={item[1]}/>
-        });
+        const {loading, message, restaurantName, itemsToQuantity, totalPrice, emptyCart} = this.state;
+
+        let itemComponentList;
+
+        if (!emptyCart) {
+            itemComponentList = Object.entries(itemsToQuantity).map(function (item) {
+                console.log("Item: ", item[0])
+                return <CartItem key={JSON.parse(item[0]).name} foodItem={item[0]}
+                                 quantity={item[1]}/>
+            });
+        } else {
+            itemComponentList = <div/>
+        }
 
 
         return (
@@ -101,15 +119,34 @@ export default class Cart extends Component {
                         </Fragment>
                     )}
 
-                    {!loading && (
+                    {!loading && !emptyCart && (
                         <Fragment>
                             <h3>From Restaurant: {restaurantName}</h3>
                             <h4>Total Price: {totalPrice}</h4>
+
+                            <Form
+                                onSubmit={this.handlePlaceOrder}
+                                ref={c => {
+                                    this.form = c;
+                                }}
+                                history={this.props.history}
+                            >
+                                <div className="text-center">
+                                    <button className="btn btn-primary btn-block">
+                                        <span>Place Order</span>
+                                    </button>
+                                </div>
+                            </Form>
+
                             <h4>Items:</h4>
                             <div>
                                 {itemComponentList}
                             </div>
                         </Fragment>
+                    )}
+
+                    {emptyCart && (
+                        <p>Your cart is currently empty. Explore our extensive catalogue of <a href={"/restaurants"}>restaurants</a> to find the most delicius foods!</p>
                     )}
                 </div>
             </div>
