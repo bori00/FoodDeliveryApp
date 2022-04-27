a122496ad453b6a2b276bd9154e04f5167edb306
bori00 <fazakasbori@gmail.com>
2022-04-28 01:01:22 +0300

    generate full pdf for the menu

--- src/main/java/com/example/food_delivery/controller/MenuAccessController.java
+++ src/main/java/com/example/food_delivery/controller/MenuAccessController.java
@@ -1,11 +1,18 @@
 package com.example.food_delivery.controller;
 
+import com.example.food_delivery.model.DTO.FoodDTO;
+import com.example.food_delivery.model.Food;
+import com.example.food_delivery.model.FoodCategory;
 import com.example.food_delivery.service.menu_pdf_generation.MenuPDFGenerationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class MenuAccessController {
@@ -16,7 +23,17 @@ public class MenuAccessController {
     @GetMapping("/get_my_menu_in_pdf")
     @PreAuthorize("hasAuthority('RESTAURANT_ADMIN')")
     public InputStreamResource getMyRestaurantsMenuInPdf() {
-        menuPDFGenerationService.createMenuPDF();
+        Map<FoodCategory, List<FoodDTO>> map = new HashMap<>();
+
+        FoodCategory appetizer = new FoodCategory(1L, "appetizer");
+        map.put(appetizer, List.of(new FoodDTO("Food1", 10.0, null, "appetizer"),
+                new FoodDTO("Food2", 30.0, "d2", "appetizer")));
+
+        FoodCategory main = new FoodCategory(1L, "main course");
+        map.put(main, List.of(new FoodDTO("Food1", 10.0, "", "appetizer"),
+                new FoodDTO("Food2", 30.0, "d2", "appetizer")));
+
+        menuPDFGenerationService.createMenuPDF("Little Italy", map);
         return null;
     }
 }
--- src/main/java/com/example/food_delivery/service/menu_pdf_generation/MenuPDFGenerationService.java
+++ src/main/java/com/example/food_delivery/service/menu_pdf_generation/MenuPDFGenerationService.java
@@ -1,7 +1,11 @@
 package com.example.food_delivery.service.menu_pdf_generation;
 
+import com.example.food_delivery.model.DTO.FoodDTO;
+import com.example.food_delivery.model.Food;
+import com.example.food_delivery.model.FoodCategory;
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.draw.DottedLineSeparator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -11,15 +15,19 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class MenuPDFGenerationService {
 
     private static final Font TITLE_FONT = new Font(Font.FontFamily.TIMES_ROMAN, 36,
             Font.BOLD);
-    private static final Font SMALL_BOLD_FONT = new Font(Font.FontFamily.TIMES_ROMAN, 12,
+    private static final Font SUBTITLE_FONT = new Font(Font.FontFamily.TIMES_ROMAN, 22,
             Font.BOLD);
-    private static final Font SMALL_FONT = new Font(Font.FontFamily.TIMES_ROMAN, 10);
+    private static final Font SMALL_BOLD_FONT = new Font(Font.FontFamily.TIMES_ROMAN, 16,
+            Font.BOLD);
+    private static final Font SMALL_FONT = new Font(Font.FontFamily.TIMES_ROMAN, 14);
 
     private static final String LOGO_PATH = "src/main/resources/img/menu.png";
 
@@ -30,7 +38,8 @@ public class MenuPDFGenerationService {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(
             "yyyy_MM_dd-HH_mm_ss");
 
-    public void createMenuPDF()  {
+    public void createMenuPDF(String restaurantName,
+                              Map<FoodCategory, List<FoodDTO>> categoriesToFoodList)  {
         try {
             Document document = new Document();
             ByteArrayOutputStream out = new ByteArrayOutputStream();
@@ -38,9 +47,8 @@ public class MenuPDFGenerationService {
             PdfWriter.getInstance(document, new FileOutputStream(String.format(FILE_PATH_FORMAT,
                     LocalDateTime.now().format(DATE_TIME_FORMATTER))));
             document.open();
-            addMetaData(document);
-            addContent(document);
-            //  addContent(document, client, product, order);
+            addMetaData(document, restaurantName);
+            addContent(document, restaurantName, categoriesToFoodList);
             document.close();
         } catch (DocumentException | IOException e) {
            // LOGGER.error(Throwables.getStackTraceAsString(e));
@@ -48,33 +56,93 @@ public class MenuPDFGenerationService {
         }
     }
 
-    private static void addMetaData(Document document) {
-        document.addTitle("Restaurant Menu");
+
+
+    private void addMetaData(Document document, String restaurantName) {
+        document.addTitle(String.format("%s's Menu", restaurantName));
         document.addSubject("menu");
     }
 
-    private static void addContent(Document document)
+    private void addContent(Document document,
+                            String restaurantName,
+                            Map<FoodCategory, List<FoodDTO>> categoriesToFoodList)
             throws DocumentException, IOException {
-        addPrefaceParagraph(document);
+        addPrefaceParagraph(document, restaurantName);
+
+        for (FoodCategory foodCategory : categoriesToFoodList.keySet()) {
+            addFoodCategorySection(document, foodCategory, categoriesToFoodList.get(foodCategory));
+        }
     }
 
-    private static void addPrefaceParagraph(Document document) throws DocumentException, IOException {
+    private void addPrefaceParagraph(Document document, String restaurantName) throws DocumentException, IOException {
         Paragraph preface = new Paragraph();
         Image image = Image.getInstance(LOGO_PATH);
         image.setAlignment(Element.ALIGN_CENTER);
         image.scaleAbsolute(100, 100);
         preface.add(image);
         addEmptyLine(preface, 1);
-        Paragraph titlePara = new Paragraph("Menu", TITLE_FONT);
+        Paragraph titlePara = new Paragraph(String.format("%s's Menu", restaurantName), TITLE_FONT);
         titlePara.setAlignment(Element.ALIGN_CENTER);
         preface.add(titlePara);
         addEmptyLine(preface, 1);
         document.add(preface);
     }
 
-    private static void addEmptyLine(Paragraph paragraph, int number) {
+    private void addFoodCategorySection(Document document,
+                                        FoodCategory foodCategory,
+                                        List<FoodDTO> foodList) throws DocumentException {
+        document.newPage();
+
+        Paragraph section = new Paragraph();
+
+        DottedLineSeparator dottedline = new DottedLineSeparator();
+        dottedline.setOffset(-2);
+        dottedline.setGap(2f);
+        section.add(dottedline);
+
+        addEmptyLine(section, 1);
+
+        Paragraph titlePara = new Paragraph(foodCategory.getName() + "s", SUBTITLE_FONT);
+        titlePara.setAlignment(Element.ALIGN_CENTER);
+        section.add(titlePara);
+
+        addEmptyLine(section, 1);
+
+        document.add(section);
+
+        for (FoodDTO foodDTO : foodList) {
+            addFoodSubsection(document, foodDTO);
+        }
+    }
+
+    private void addFoodSubsection(Document document, FoodDTO foodDTO) throws DocumentException {
+        Paragraph section = new Paragraph();
+        section.setIndentationLeft(40);
+
+        Paragraph titlePara = new Paragraph(foodDTO.getName(), SMALL_BOLD_FONT);
+        titlePara.setAlignment(Element.ALIGN_LEFT);
+        section.add(titlePara);
+
+        Paragraph pricePara = new Paragraph(String.format("Price: %s$", foodDTO.getPrice()), SMALL_FONT);
+        pricePara.setAlignment(Element.ALIGN_LEFT);
+        section.add(pricePara);
+
+        if (foodDTO.getDescription() != null && !foodDTO.getDescription().isBlank()) {
+            Paragraph descriptionPara = new Paragraph(String.valueOf(foodDTO.getDescription()),
+                    SMALL_FONT);
+            descriptionPara.setAlignment(Element.ALIGN_LEFT);
+            section.add(descriptionPara);
+        }
+
+        addEmptyLine(section, 1);
+
+        document.add(section);
+    }
+
+    private void addEmptyLine(Paragraph paragraph, int number) {
         for (int i = 0; i < number; i++) {
             paragraph.add(new Paragraph(" "));
         }
     }
+
 }
