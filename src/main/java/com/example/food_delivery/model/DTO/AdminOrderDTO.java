4f8421378ecde85dc0f897dfdaffe6f3ab41069d
bori00 <fazakasbori@gmail.com>
2022-04-30 13:50:20 +0300

    setup testing

--- src/main/java/com/example/food_delivery/model/DTO/AdminOrderDTO.java
+++ src/main/java/com/example/food_delivery/model/DTO/AdminOrderDTO.java
@@ -33,4 +33,30 @@ public class AdminOrderDTO {
 
     @JsonSerialize(keyUsing = CustomerOrderDTO.FoodDTOSerializer.class)
     private Map<FoodDTO, Integer> orderedItemsToQuantity;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AdminOrderDTO that = (AdminOrderDTO) o;
+
+        if (Id != null ? !Id.equals(that.Id) : that.Id != null) return false;
+        if (clientName != null ? !clientName.equals(that.clientName) : that.clientName != null)
+            return false;
+        if (status != null ? !status.equals(that.status) : that.status != null) return false;
+        if (dateTime != null ? !dateTime.equals(that.dateTime) : that.dateTime != null)
+            return false;
+        return orderedItemsToQuantity != null ? orderedItemsToQuantity.equals(that.orderedItemsToQuantity) : that.orderedItemsToQuantity == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Id != null ? Id.hashCode() : 0;
+        result = 31 * result + (clientName != null ? clientName.hashCode() : 0);
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        result = 31 * result + (dateTime != null ? dateTime.hashCode() : 0);
+        result = 31 * result + (orderedItemsToQuantity != null ? orderedItemsToQuantity.hashCode() : 0);
+        return result;
+    }
 }
